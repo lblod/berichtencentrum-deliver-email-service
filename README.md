65dc8c1d60c46e77310d8636c77b44e81e35ed46
Claire Lovisa <lovisa.claire@gmail.com>
2018-12-06 13:48:53 +0100

    Send a stub email to a stub receiver via Gmail

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # berichtencentrum-deliver-email-service
 Microservice that delivers messages as emails
+
+### Environment variables
+```
+  GMAIL_ADDRESS: required, 'aGmailAddress'
+  GMAIL_PASSWORD: required, 'theCorrespondingPassword'
+```
--- app.js
+++ app.js
@@ -1,1 +1,42 @@
-/* Todo */
+import {
+  app,
+  errorHandler
+} from 'mu';
+
+const nodemailer = require('nodemailer');
+
+app.get('/', async function(req, res) {
+  res.send('Hello from deliver-bbcdr-rapporten-service');
+});
+
+let transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: process.env.GMAIL_ADDRESS,
+    pass: process.env.GMAIL_PASSWORD
+  }
+});
+
+let mailOptions = {
+  from: '"Claire Lovisa" <claire.lovisa@redpencil.io>',
+  to: 'claire.lovisa@redpencil.io',
+  subject: 'Hello ✔',
+  text: 'Hello world?',
+  html: '<b>Hello world?</b>',
+  attachments: [
+    {
+      filename: 'hello.txt',
+      content: 'Hello world !'
+    }
+  ]
+};
+
+transporter.sendMail(mailOptions, (error, info) => {
+  if (error) {
+    return console.log(error);
+  }
+  console.log('Message sent: %s', info.messageId);
+  console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+});
+
+app.use(errorHandler);
